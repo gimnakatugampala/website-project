5e2b23272df65ff59a8905300318e0089becc24a
gimnakatugampala <gimnakatugampala1@gmail.com>
2022-01-13 19:12:58 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+import { BrowserRouter as Router,Route , Routes } from 'react-router-dom';
 import Welcome from './components/Welcome/Welcome';
 import SignUp from './components/SignUp/SignUp';
 import MainLogin from './components/LogIn/MainLogin';
@@ -10,12 +11,16 @@ import UpdatePassword from './components/UpdatePassword/UpdatePassword';
 function App() {
   return (
     <div>
+      <Router>
+        <Routes>
+        <Route path="/" element={<Welcome />} />
+        </Routes>
+      </Router>
      {/* <Welcome /> */}
      {/* <SignUp /> */}
      {/* <MainLogin /> */}
      {/* <SecondLogin /> */}
      {/* <ThirdLogin /> */}
-     <UpdatePassword />
     </div>
   );
 }
